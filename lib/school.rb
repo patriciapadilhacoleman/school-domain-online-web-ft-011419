d941e06db88e655e2066ea38bd79cf7fc0bc5d92
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-01-26 22:22:30 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
 # code here!
+class School
+  
+  attr_accessor :roster
+  attr_reader :name
+  
+  def initialize(name)
+    
+    @name = name
+    @roster = {}
+    
+  end
+  
+  def add_student(name, grade)
+    
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+    @roster[grade]<< name
+    
+  end
+  
+  def grade(grade)
+    
+     @roster.values_at(grade).flatten
+     
+    
+  end
+  
+  def sort
+    
+    temproster = {}
+    
+    @roster.each do |grade, students| 
+      
+      temproster[grade] =[]
+      temproster[grade] = students.sort
+    
+    end
+    
+    @roster = temproster
+  
+  end
+  
+end
